21513da97d6647fa67e18f6426e76457262c84be
feiyu <lvp123321123321@gmail.com>
2026-06-07 15:13:06 +0700

    sync addition mining algo + params with core node

--- cpp/public_settings.hpp
+++ cpp/public_settings.hpp
@@ -18,12 +18,13 @@ static constexpr unsigned long long HYPERIDENTITY_NUMBER_OF_MUTATIONS = 150;
 static constexpr unsigned long long HYPERIDENTITY_POPULATION_THRESHOLD = HYPERIDENTITY_NUMBER_OF_INPUT_NEURONS + HYPERIDENTITY_NUMBER_OF_OUTPUT_NEURONS + HYPERIDENTITY_NUMBER_OF_MUTATIONS; // P
 static constexpr unsigned int HYPERIDENTITY_SOLUTION_THRESHOLD_DEFAULT = 321;
 
-static constexpr unsigned long long ADDITION_NUMBER_OF_INPUT_NEURONS = 14; // K
-static constexpr unsigned long long ADDITION_NUMBER_OF_OUTPUT_NEURONS = 8; // L
-static constexpr unsigned long long ADDITION_NUMBER_OF_TICKS = 1000;       // N
-static constexpr unsigned long long ADDITION_NUMBER_OF_NEIGHBORS = 728;    // 2M. Must be divided by 2
-static constexpr unsigned long long ADDITION_NUMBER_OF_MUTATIONS = 500;
-static constexpr unsigned long long ADDITION_POPULATION_THRESHOLD = ADDITION_NUMBER_OF_INPUT_NEURONS + ADDITION_NUMBER_OF_OUTPUT_NEURONS + ADDITION_NUMBER_OF_MUTATIONS; // P
-static constexpr unsigned int ADDITION_SOLUTION_THRESHOLD_DEFAULT = 75700;
+static constexpr unsigned long long ADDITION_NUMBER_OF_INPUT_NEURONS = 14;
+static constexpr unsigned long long ADDITION_NUMBER_OF_OUTPUT_NEURONS = 8;
+static constexpr unsigned long long ADDITION_NUMBER_OF_TICKS = 256;
+static constexpr unsigned long long ADDITION_POPULATION_THRESHOLD = 256;
+// Each neuron is connected to every other neuron(exclude self). The effective is clamp to (ADDITION_POPULATION_THRESHOLD - 1) at runtime
+static constexpr unsigned long long ADDITION_NUMBER_OF_NEIGHBORS = ADDITION_POPULATION_THRESHOLD;
+static constexpr unsigned long long ADDITION_NUMBER_OF_MUTATIONS = 256;
+static constexpr unsigned int ADDITION_SOLUTION_THRESHOLD_DEFAULT = 76000;
 
 static constexpr long long NEURON_VALUE_LIMIT = 1LL;
